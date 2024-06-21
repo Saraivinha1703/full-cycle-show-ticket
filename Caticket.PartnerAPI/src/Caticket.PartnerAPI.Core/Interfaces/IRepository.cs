bbcb07ab0e132a007b7c5b005c476fd9c3ab2b6d
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-06-21 14:04:34 +0100

    📦 NEW: Changing IEnumerable to IQueryable, adding IBase interface as T type and passing T to create Task

--- Caticket.PartnerAPI/src/Caticket.PartnerAPI.Core/Interfaces/IRepository.cs
+++ Caticket.PartnerAPI/src/Caticket.PartnerAPI.Core/Interfaces/IRepository.cs
@@ -1,8 +1,8 @@
 namespace Caticket.PartnerAPI.Core.Interfaces;
 
-public interface IRepository<T> : IDisposable where T : class {
-    Task CreateAsync();
-    Task<IEnumerable<T>> GetAllAsync();
+public interface IRepository<T> : IDisposable where T : class, IBase {
+    Task CreateAsync(T entity);
+    Task<IQueryable<T>> GetAllAsync();
     Task<T> GetByIdAsync(Guid id);
     Task UpdateAsync(Guid id, T entity);
     Task DeleteAsync(Guid id);
