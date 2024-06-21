27362754203dfd22ac431952bf9dbbad8cc16017
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-06-21 14:08:19 +0100

    📦 NEW: Starting infrastructure services configuration

--- Caticket.PartnerAPI/src/Caticket.PartnerAPI.Infrastructure/Services/InfrastructureConfiguration.cs
+++ Caticket.PartnerAPI/src/Caticket.PartnerAPI.Infrastructure/Services/InfrastructureConfiguration.cs
@@ -1,5 +1,7 @@
 using Caticket.PartnerAPI.Core.Entities;
+using Caticket.PartnerAPI.Core.Interfaces;
 using Caticket.PartnerAPI.Infrastructure.Data;
+using Caticket.PartnerAPI.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,6 +9,12 @@ namespace Caticket.PartnerAPI.Infrastructure.Services;
 
 public static class InfrastructureConfiguration {
     public static void Configure(IServiceCollection services) {
-        services.AddDbContext<DatabaseContext<Event>>(options => options.UseMySQL());
+        //TODO: Implement generic dbContext, preferred to add a Program.cs file in the Infrastructure, so the migrations and anything related to the database only happens here.
+        services.AddDbContext<DatabaseContext<object>>(options => options.UseMySQL("server=localhost;port=3307;database=api;uid=root;password=root"));
+
+        services.AddScoped<IRepository<Event>, EventRepository>();
+        services.AddScoped<IRepository<ReservationHistory>, ReservationHistoryRepository>();
+        services.AddScoped<IRepository<Spot>, SpotRepository>();
+        services.AddScoped<IRepository<Ticket>, TicketRepository>();
     }
 }
