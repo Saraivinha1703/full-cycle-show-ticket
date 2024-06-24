96485d6b7afbc831dfcc597aef86d59958ebf55d
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-06-24 15:12:37 +0100

    📦 NEW: Making CRUD operations

--- Caticket.PartnerAPI/src/Caticket.PartnerAPI.Web/Endpoints/EventEndpoint.cs
+++ Caticket.PartnerAPI/src/Caticket.PartnerAPI.Web/Endpoints/EventEndpoint.cs
@@ -1,28 +1,52 @@
+using System.Reflection;
 using Caticket.PartnerAPI.Core.Services;
 using Caticket.PartnerAPI.Web.DTO;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Caticket.PartnerAPI.Web.Endpoints;
 
-public record Dto(string Name);
+public class Dto {
+    public string? Name { get; set; }
+
+    public static ValueTask<Dto?> BindAsync(HttpContext context, ParameterInfo parameter) {
+        string? name = context.Request.Query["Name"];
+
+        return ValueTask.FromResult<Dto?>(new() { Name = name });
+    }
+};
+
 public static class EventEndpoint {
     public static void MapEventEndpoints(this WebApplication app) {
-        app.MapGet("/event", () => "Hello from Events");
+        app.MapGet(
+            "/event", 
+            async (Dto dto, [FromServices] EventService eventService) => {
+                if(dto.Name == null) return await eventService.GetAllEvents();
+            
+                return await eventService.GetEventByName(dto.Name);
+            }
+        );
         
         app.MapPost(
             "/event", 
-            async ([FromBody] CreateEventDto eventDto, [FromServices] EventService _eventService) => {
-                await _eventService.CreateEvent(eventDto);
+            async ([FromBody] CreateEventDto eventDto, [FromServices] EventService eventService) => {
+                await eventService.CreateEvent(eventDto);
                 return "event created!";
             }
         );
 
-        app.MapPost(
-            "/event-name", 
-            async ([FromBody] Dto dto, [FromServices] EventService _eventService) => {
-                Console.WriteLine("Name: " + dto.Name);
-                return await _eventService.GetEventByName(dto.Name);
+        app.MapPatch(
+            "/event", 
+            async ([FromBody] UpdateEventDto updateEventDto, [FromServices] EventService eventService) => {
+                await eventService.Update(updateEventDto);
+                return "event updated!";
             }
         );
+
+        app.MapDelete(
+            "/event", 
+            async ([FromBody] BaseDeleteDto deleteDto, [FromServices] EventService eventService) => {
+                await eventService.Delete(deleteDto.Id);
+                return "event deleted!";
+            });
     }
 }
