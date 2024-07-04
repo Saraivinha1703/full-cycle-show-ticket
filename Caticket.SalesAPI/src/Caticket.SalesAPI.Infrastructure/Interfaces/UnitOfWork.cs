5d8582bbc9f8c6943164575d31753098d2f7fd10
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-07-04 16:35:07 +0100

    📦 NEW: Adding generic repository pattern necessary files

---
+++ Caticket.SalesAPI/src/Caticket.SalesAPI.Infrastructure/Interfaces/UnitOfWork.cs
@@ -0,0 +1,17 @@
+// using Caticket.PartnerAPI.Domain.Interfaces;
+// using Caticket.PartnerAPI.Infrastructure.Data;
+
+// namespace Caticket.PartnerAPI.Infrastructure.Interfaces;
+
+// public class UnitOfWork(DatabaseContext dbContext) : IUnitOfWork
+// {
+//     private readonly DatabaseContext _dbContext = dbContext;
+
+//     public async Task<bool> Commit() => (await _dbContext.SaveChangesAsync()) > 0;
+
+//     public Task Rollback()
+//     {
+//         //Implement any rollback logic if needed
+//         return Task.CompletedTask;
+//     }
+// }
