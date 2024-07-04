26c2e231dfe982bc6257ca7385f684e35b103c11
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-07-04 16:33:56 +0100

    📦 NEW: Matching the model change

--- Caticket.SalesAPI/src/Caticket.SalesAPI.Core/Services/EventService.cs
+++ Caticket.SalesAPI/src/Caticket.SalesAPI.Core/Services/EventService.cs
@@ -1,9 +1,10 @@
 using Caticket.SalesAPI.Domain.Entities;
+using Caticket.SalesAPI.Domain.Enumerators;
 
 namespace Caticket.SalesAPI.Core.Services;
 
 public class EventService {
     public Task<Spot> AddSpot(string name) {
-        return Task.FromResult(new Spot("", Guid.NewGuid(), Guid.NewGuid()));
+        return Task.FromResult(new Spot("", Guid.NewGuid(), SpotStatus.Available));
     }
 }
