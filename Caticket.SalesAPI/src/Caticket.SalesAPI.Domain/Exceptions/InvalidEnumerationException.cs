399ba74649e841c8346929a84bf7ae099fdf00e4
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-07-04 18:00:14 +0100

    📦 NEW: Adding custom exception

---
+++ Caticket.SalesAPI/src/Caticket.SalesAPI.Domain/Exceptions/InvalidEnumerationException.cs
@@ -0,0 +1,8 @@
+namespace Caticket.SalesAPI.Domain.Exceptions;
+
+public class InvalidEnumerationException(
+    string? message = null,
+    Exception? innerException = null
+) : Exception($"{message}: This key is invalid, check if it matches the key in the enumerator.", innerException) {
+
+}
