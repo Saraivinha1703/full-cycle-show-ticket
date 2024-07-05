c54075d60cc2900aba1739374c7f4883bc1a3327
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-07-05 13:42:47 +0100

    📦 NEW: object to implement database connection dynamicaly

---
+++ Caticket.PartnerAPI/src/Caticket.PartnerAPI.Infrastructure/Interfaces/DatabaseConnectionInfo.cs
@@ -0,0 +1,7 @@
+namespace Caticket.PartnerAPI.Infrastructure.Interfaces;
+
+public class DatabaseConnectionInfo
+{
+    public string Assembly { get; set; } = null!;
+    public string ConnectionString { get; set; } = null!;
+}
