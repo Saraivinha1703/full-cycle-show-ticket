9e90a708e690fc9ad565a0c7caa0c618b3ad75ab
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-07-05 13:42:21 +0100

    📦 NEW: trying to make database access variable

--- Caticket.PartnerAPI/src/Caticket.PartnerAPI.Infrastructure/Data/DatabaseContext.cs
+++ Caticket.PartnerAPI/src/Caticket.PartnerAPI.Infrastructure/Data/DatabaseContext.cs
@@ -1,28 +1,27 @@
 using Caticket.PartnerAPI.Domain.Entities;
+using Caticket.PartnerAPI.Infrastructure.Interfaces;
 using Caticket.PartnerAPI.Infrastructure.Mappers;
 using Microsoft.EntityFrameworkCore;
 
 namespace Caticket.PartnerAPI.Infrastructure.Data;
 
 public class DatabaseContext : DbContext { 
-    // public DbSet<Event> Events { get; set; }
-    // public DbSet<ReservationHistory> ReservationHistories { get; set; }
-    // public DbSet<Spot> Spots { get; set; }
-    // public DbSet<Ticket> Tickets { get; set; }
-
-    public DatabaseContext() {}   
+    public DatabaseContext() { }   
     public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) {}
+
     protected override void OnConfiguring(DbContextOptionsBuilder options) {
         var serverVersion = new MySqlServerVersion(new Version(8, 0, 30));
         //running on container
         // var connectionString = "server=partner-db;user=root;password=root;database=api";
-        //running locally
-        var connectionString = "server=localhost;user=root;password=root;database=api;port=3307";
 
+        //running locally
+        //var connectionString = "server=localhost;user=root;password=root;database=api;port=3307";
+        var dbInfo = new DatabaseConnectionInfo();
+        
         options.UseMySql(
-            connectionString, 
+            dbInfo.ConnectionString, 
             serverVersion, 
-            o => o.MigrationsAssembly("Caticket.PartnerAPI.Web")
+            o => o.MigrationsAssembly(dbInfo.Assembly)
         );
     }
     protected override void OnModelCreating(ModelBuilder modelBuilder) {
