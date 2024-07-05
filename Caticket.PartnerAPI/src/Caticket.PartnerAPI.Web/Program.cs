b3eaedad6d0365307adebf86b1be1eacad51f310
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-07-05 13:49:23 +0100

    📦 NEW: Database connection dependency injection

--- Caticket.PartnerAPI/src/Caticket.PartnerAPI.Web/Program.cs
+++ Caticket.PartnerAPI/src/Caticket.PartnerAPI.Web/Program.cs
@@ -1,5 +1,6 @@
 using Caticket.PartnerAPI.Core.Services;
 using Caticket.PartnerAPI.Infrastructure.Data;
+using Caticket.PartnerAPI.Infrastructure.Interfaces;
 using Caticket.PartnerAPI.Infrastructure.Services;
 using Caticket.PartnerAPI.Web.Endpoints;
 using Caticket.PartnerAPI.Web.Middlewares;
@@ -10,10 +11,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/documentation-comments
+var config = new ConfigurationManager();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.Configure<DatabaseConnectionInfo>(options => config.GetSection("DatabaseConnectionInfo").Bind(options));
 builder.Services.ConfigureDatabase();
 builder.Services.AddCoreServices();
 
