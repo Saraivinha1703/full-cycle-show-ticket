030ed3425cfd5d79b96335307ed3f1f0581ece9c
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-07-05 13:41:25 +0100

    🐛 FIX: Multi tenant filter will be implemented

--- Caticket.PartnerAPI/src/Caticket.ParterAPI.Web2/DTO/BaseDeleteRequest.cs
+++
@@ -1,4 +0,0 @@
-
-namespace Caticket.PartnerAPI.Web2.DTO;
-
-public record BaseDeleteRequest(Guid Id);
--- Caticket.PartnerAPI/src/Caticket.ParterAPI.Web2/DTO/Spot/CreateSpoRequest.cs
+++
@@ -1,4 +0,0 @@
-
-namespace Caticket.PartnerAPI.Web2.DTO.Spot;
-
-public record CreateSpotRequest(string Name);
