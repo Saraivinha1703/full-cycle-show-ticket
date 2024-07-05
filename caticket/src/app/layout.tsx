65c3ddbeebe911555ec25be3904189615cc446c7
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-07-05 14:39:08 +0100

    📦 NEW: front end project initialized

---
+++ caticket/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Caticket",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.ico" sizes="any" />
+      </head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
