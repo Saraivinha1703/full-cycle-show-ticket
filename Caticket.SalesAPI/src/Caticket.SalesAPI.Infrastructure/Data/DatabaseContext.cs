8fb05e5794b5cd4cddf31f52023d19af11a7aa9f
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-07-15 17:42:45 +0100

    📦 NEW: adding db connection

--- Caticket.SalesAPI/src/Caticket.SalesAPI.Infrastructure/Data/DatabaseContext.cs
+++ Caticket.SalesAPI/src/Caticket.SalesAPI.Infrastructure/Data/DatabaseContext.cs
@@ -4,5 +4,19 @@ namespace Caticket.SalesAPI.Infrastructure.Data;
 
 public class DatabaseContext : DbContext
 {
-    
+     public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) {}
+    public DatabaseContext() {}
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string connectionString = "server=localhost;user=root;password=root;database=api;port=3308";
+        //ServerVersion sv = new MySqlServerVersion(new Version(8, 0, 30));
+
+        optionsBuilder.UseMySql(
+            connectionString, 
+            ServerVersion.AutoDetect(connectionString), 
+            msql => msql.MigrationsAssembly("Caticket.SalesAPI.Web")
+        );
+        base.OnConfiguring(optionsBuilder);
+    }
 }
