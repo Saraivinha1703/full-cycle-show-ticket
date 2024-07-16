0dd5ba3a561130eda392cf8051874ed18aaac882
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-07-16 16:50:02 +0100

    📦 NEW: Testing identity service through an endpoint

--- Caticket.SalesAPI/src/Caticket.SalesAPI.Web/Endpoints/Authentication/UserEndpoints.cs
+++ Caticket.SalesAPI/src/Caticket.SalesAPI.Web/Endpoints/Authentication/UserEndpoints.cs
@@ -1,8 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Caticket.SalesAPI.Application.DTOs.Request;
+using Caticket.SalesAPI.Application.Interfaces.Services;
+using Caticket.SalesAPI.Application.DTOs.Response;
+
 namespace Caticket.SalesAPI.Web.Endpoints.Authentication;
 
 public static class UserEndpoints {
     public static void MapUserEndpoints(this WebApplication app) {
-        app.MapPost("/register/partner", () => {return "partner user";});
+        app.MapPost(
+            "/register/partner", 
+            async (
+                [FromBody] UserSignUpRequest userDto, 
+                [FromServices] IIdentityService identityService
+            ) => {
+                var result = await identityService.SignUpAsync(userDto);
+
+                if(!result.Success)
+                    return Results.BadRequest(result);
+
+                return Results.Ok(result);
+            }
+        );
         app.MapPost("/register/user", () => {return "common user";});
         app.MapPost("/login", () => {return "login";});
     }
