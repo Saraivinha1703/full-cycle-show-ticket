ffbaf425344882612b4c3d23c6c5ee0d6415e56b
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-07-16 17:59:01 +0100

    📦 NEW: Creating validator with FluentValidation

---
+++ Caticket.SalesAPI/src/Caticket.SalesAPI.Application/Validators/UserSignUpValidator.cs
@@ -0,0 +1,35 @@
+using Caticket.SalesAPI.Application.DTOs.Request;
+using FluentValidation;
+
+namespace Caticket.SalesAPI.Application.Validators;
+
+public class UserSignUpValidator : AbstractValidator<UserSignUpRequest> {
+    public UserSignUpValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .WithMessage("The email field is mandatory.")
+            .EmailAddress()
+            .WithMessage("The email is invalid, type a valid email format.");
+        
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("The name field is mandatory.");
+        
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .WithMessage("The password field is mandatory.")
+            .MinimumLength(6)
+            .WithMessage("The password must have more than 6 digits.")
+            .MaximumLength(55)
+            .WithMessage("The password must have less than 55 digits.")
+            .Must(s => s.Any(char.IsUpper))
+            .WithMessage("The password must have upper case letter.")
+            .Must(s => s.Any(char.IsLower))
+            .WithMessage("The password must have lower case letter.");
+
+        RuleFor(x => x.ConfirmPassword)
+            .Matches(y => y.Password)
+            .WithMessage("The confirm password must match the password field.");
+    }
+}
