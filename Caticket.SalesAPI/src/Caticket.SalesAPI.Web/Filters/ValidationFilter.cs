f4dd2079e9da57b955764c543c9327db97ae44ac
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-07-16 18:00:39 +0100

    📦 NEW: Making a generic validation endpoint filter

---
+++ Caticket.SalesAPI/src/Caticket.SalesAPI.Web/Filters/ValidationFilter.cs
@@ -0,0 +1,21 @@
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace Caticket.SalesAPI.Web.Filters;
+
+public class ValidationFilter<T>(IValidator<T> validator) : IEndpointFilter where T : class {
+    private readonly IValidator<T> _validator = validator;
+
+    public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+    {
+        if (context.Arguments.FirstOrDefault(a => a?.GetType() == typeof(T)) is not T dto) 
+            return await next(context);
+
+        ValidationResult result = await _validator.ValidateAsync(dto);
+
+        if(!result.IsValid) 
+            return Results.Json(result.Errors, statusCode: StatusCodes.Status400BadRequest);
+        
+        return await next(context);
+    }
+}
