96519af94261411885efc0c02215afd48115698a
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-07-17 11:04:53 +0100

    🐛 FIX: passing wrong parameter to errors list

--- Caticket.SalesAPI/src/Caticket.SalesAPI.Application/DTOs/Response/BaseResponse.cs
+++ Caticket.SalesAPI/src/Caticket.SalesAPI.Application/DTOs/Response/BaseResponse.cs
@@ -6,9 +6,9 @@ public abstract class BaseResponse : IResponse
 {
     public bool Success { get; protected set; }
 
-    public List<string>? Errors { get; protected set; }
+    public List<string> Errors { get; protected set; } = [];
 
-    public void AddError(string error) => Errors?.Add(error);
+    public void AddError(string error) => Errors.Add(error);
 
-    public void AddErrors(IEnumerable<string> errors) => Errors?.AddRange(Errors);
+    public void AddErrors(IEnumerable<string> errors) => Errors.AddRange(errors);
 }
