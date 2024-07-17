87d6eb061cae6779346987e237e9913e918d9edc
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-07-17 11:10:27 +0100

    📦 NEW: removing unnecessary constructor

--- Caticket.SalesAPI/src/Caticket.SalesAPI.Application/DTOs/Response/UserSignUpResponse.cs
+++ Caticket.SalesAPI/src/Caticket.SalesAPI.Application/DTOs/Response/UserSignUpResponse.cs
@@ -3,6 +3,5 @@ using Caticket.SalesAPI.Application.Interfaces.DTOs;
 namespace Caticket.SalesAPI.Application.DTOs.Response;
 
 public class UserSignUpResponse : BaseResponse, IResponse {
-    public UserSignUpResponse() => Errors = [];
     public UserSignUpResponse(bool success) => Success = success;
 }
