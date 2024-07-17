ec74fb22d3690f9d13a22103a82228bf0308a715
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-07-17 11:05:27 +0100

    📦 NEW: removing unnecessary constructor

--- Caticket.SalesAPI/src/Caticket.SalesAPI.Application/DTOs/Response/UserLoginResponse.cs
+++ Caticket.SalesAPI/src/Caticket.SalesAPI.Application/DTOs/Response/UserLoginResponse.cs
@@ -10,8 +10,6 @@ public class UserLoginResponse : BaseResponse, IResponse {
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public DateTime? ExpirationDate { get; private set; } = null;
 
-    public UserLoginResponse() => Errors = [];
-
     public UserLoginResponse(bool success) => Success = success;
 
     public UserLoginResponse(bool success, DateTime expirationDate, string token) {
