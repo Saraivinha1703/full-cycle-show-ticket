88e7e6f6c937700dd125fa640c1448f8d1ada5c0
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-07-17 11:04:20 +0100

    📦 NEW: Removing login request dto data annotations

--- Caticket.SalesAPI/src/Caticket.SalesAPI.Application/DTOs/Request/UserLoginRequest.cs
+++ Caticket.SalesAPI/src/Caticket.SalesAPI.Application/DTOs/Request/UserLoginRequest.cs
@@ -1,13 +1,6 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace Caticket.SalesAPI.Application.DTOs.Request;
 
 public class UserLoginRequest {
-    [Required(ErrorMessage = "The field {0} is mandatory.")]
-    [EmailAddress(ErrorMessage = "The field {0} is invalid, type a valid email.")]
     public required string Email { get; set; }
-
-    [Required(ErrorMessage = "The field {0} is mandatory.")]
-    [StringLength(50, ErrorMessage = "The field {0} must be between {2} and {1} characters", MinimumLength = 6)]
     public required string Password { get; set; }
 }
