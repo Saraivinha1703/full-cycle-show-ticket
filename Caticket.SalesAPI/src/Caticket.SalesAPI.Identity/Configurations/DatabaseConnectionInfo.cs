06e09de652ae038f7bc772032055663dc3b2cb8f
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-07-29 14:09:57 +0100

    📦 NEW: database configuration file

---
+++ Caticket.SalesAPI/src/Caticket.SalesAPI.Identity/Configurations/DatabaseConnectionInfo.cs
@@ -0,0 +1,7 @@
+namespace Caticket.SalesAPI.Identity.Configurations;
+
+public class DatabaseConnectionInfo
+{
+    public required string Assembly { get; set; }
+    public required string ConnectionString { get; set; }
+}
