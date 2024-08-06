5e4446e7aca77006e09d31119fe7e2d59ef9b9d5
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-08-06 17:49:15 +0100

    📦 NEW: rendering information

--- caticket/src/app/(protected)/events/fragments/partner-events.tsx
+++ caticket/src/app/(protected)/events/fragments/partner-events.tsx
@@ -1,5 +1,7 @@
+import { ShowCard } from "@/components/show-card";
 import { Button } from "@/components/ui/button";
 import { BASE_SALESAPI_URL } from "@/consts/sales-app";
+import { EventModel } from "@/models/event-model";
 
 type PartnerEventsProps = {
   token: string;
@@ -11,7 +13,7 @@ export async function PartnerEvents({ token }: PartnerEventsProps) {
     headers: { Authorization: `Bearer ${token}` },
   });
 
-  const data = await response.json();
+  const data: EventModel[] = await response.json();
 
   return (
     <main className="min-h-[calc(100vh-4rem)] p-4">
@@ -20,6 +22,24 @@ export async function PartnerEvents({ token }: PartnerEventsProps) {
         <Button>Add Event</Button>
       </div>
       {JSON.stringify(data)}
+      <div className="flex flex-wrap justify-center gap-12">
+        {data.length > 0 ? (
+          data.map((e) => (
+            <ShowCard
+              key={e.id}
+              eventId={e.id}
+              date={new Date(e.date)}
+              image={{ alt: e.name, src: /*e.imageURL*/ "" }}
+              location={e.location}
+              title={e.name}
+            />
+          ))
+        ) : (
+          <div>
+            <span>You didn&apos;t create any events yet. :(</span>
+          </div>
+        )}
+      </div>
     </main>
   );
 }
